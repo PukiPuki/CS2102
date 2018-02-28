0d13388c69502d17f82206f3d7c5b3f704acd98c
derrick <derrick.2a4@gmail.com>
2018-02-28 16:46:05 +0800

    Intiial commit

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stuffdb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+        }
+}
