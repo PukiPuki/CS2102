accacee3a3b1a5d85340242de8e8ff1ab6007a3b
DF <dfwong5566@gmail.com>
2018-03-01 13:24:53 +0800

    hello world app

--- stuffsharing/urls.py
+++ stuffsharing/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('bidding_app/',include('bidding_app.urls')),
     path('admin/', admin.site.urls),
 ]
