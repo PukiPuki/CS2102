dd9804f844316d51023fed5da75fa761abedb09e
DF <dfwong5566@gmail.com>
2018-03-01 22:07:35 +0800

    stuff

--- stuffsharing/core/views.py
+++ stuffsharing/core/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from django.http import HttpResponseRedirect
+
+from .forms import NameForm
 
 @login_required
 def home(request):
     return render(request, 'home.html')
-
 
 def signup(request):
     if request.method == 'POST':
@@ -22,3 +24,22 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    print("here")
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
